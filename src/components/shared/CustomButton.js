91b7793a3f21fd37c2ddab2fb3d3ce4ea4bd892a
Kirankumar Gonti <kirankumar225rk@gmail.com>
2021-06-23 17:21:33 +0530

    fixed matamsk image and knab button

--- src/components/shared/CustomButton.js
+++ src/components/shared/CustomButton.js
@@ -1,26 +1,29 @@
-import { 
-  makeStyles,
-  Button as MuiButton 
-} from '@material-ui/core';
+import { makeStyles, Button as MuiButton } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    // boxShadow: `0px 3px 1px -2px rgba(30, 52, 68, 0.2), 0px 2px 2px rgba(30, 52, 68, 0.14), 
+    // boxShadow: `0px 3px 1px -2px rgba(30, 52, 68, 0.2), 0px 2px 2px rgba(30, 52, 68, 0.14),
     //   0px 1px 5px rgba(30, 52, 68, 0.12)`,
-    borderRadius: '2px',
-    '&:disabled': {
+    borderRadius: "2px",
+    "&:disabled": {
       backgroundColor: theme.palette.primary.main,
-      color: '#FFF',
+      color: "#FFF",
       opacity: 0.8,
-    }
-  }
+    },
+  },
+  grabKnab: {
+    backgroundColor: theme.palette.primary.main,
+    color: "#FFF",
+    borderRadius: "33px",
+    paddingRight: "40px",
+    paddingLeft: "40px",
+  },
 }));
 
 const CustomButton = (props) => {
-
   const classes = useStyles();
 
-  const { variant, color, size, onClick, ...other } = props;
+  const { variant, color, size, onClick, knab, ...other } = props;
 
   return (
     <MuiButton
@@ -32,11 +35,11 @@ const CustomButton = (props) => {
       // disableFocusRipple
       // disableRipple
       {...other}
-      className={classes.root}
+      className={!knab ? classes.root : classes.grabKnab}
     >
       {props.children}
     </MuiButton>
   );
-}
+};
 
 export default CustomButton;
