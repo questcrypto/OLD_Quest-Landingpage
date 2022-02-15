cbacc508a52dc08e9b7f1e84bac80786a4b9808b
jcbarlow <68579151+jcbarlow@users.noreply.github.com>
2022-02-14 21:28:18 -0700

    Change IGO to DAO

--- src/components/Features.js
+++ src/components/Features.js
@@ -133,7 +133,7 @@ const Features = () => {
               </div>
               <Typography variant="h4" className={classes.title}>GOVERNANCE</Typography>
               <Typography variant="subtitle1" className={classes.subTitle2}>
-                Participate in the KNAB IGO for governance, rewards, and access to the 
+                Participate in the KNAB DAO for governance, rewards, and access to the 
                 QUEST Marketplace.
               </Typography>
               <div className={classes.btnDiv}>
